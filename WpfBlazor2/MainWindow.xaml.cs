1772a70d71963203e635e48a51072d0cda6b9d34
sepehrland <sepehr.estaki@gmail.com>
2022-11-09 13:54:49 +0330

    Add project files.

---
+++ WpfBlazor2/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Microsoft.AspNetCore.Components.WebView;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace WpfBlazor2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            var serviceCollection = new ServiceCollection();
+            serviceCollection.AddWpfBlazorWebView();
+            Resources.Add("services", serviceCollection.BuildServiceProvider());
+        }
+        private void Handle_UrlLoading(object sender,
+    UrlLoadingEventArgs urlLoadingEventArgs)
+        {
+            if (urlLoadingEventArgs.Url.Host != "0.0.0.0")
+            {
+                urlLoadingEventArgs.UrlLoadingStrategy = UrlLoadingStrategy.OpenInWebView;
+            }
+        }
+    }
+}
